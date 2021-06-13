87057000939985b4588176d8e2603b5b0616c95d
killsyre <killsyre@yahoo.com>
2021-06-13 21:42:34 +0100

    Deleted videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { VideoComponent } from './video/video.component';
 import { DropmenuComponent } from './header/dropmenu/dropmenu.component';
 import { ControlsComponent } from './video/controls/controls.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
